d8f1efa16d62f418be7f023e74b0cc5a080bb145
Hervé Torche <herve.torche@fbw-one.com>
2017-10-11 16:00:23 +0200

    My first commit

---
+++ src/pages/accueil/accueil.ts
@@ -0,0 +1,114 @@
+import {AlertController, Events, NavController} from 'ionic-angular';
+import {Component} from '@angular/core';
+// import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {EventsList} from '../events-list/events-list';
+import {Login} from '../login/login';
+import {AccountCreate} from '../account-create/account-create';
+import {AccountEdit} from '../account-edit/account-edit';
+import {PubliciteHome} from '../publicite-home/publicite-home';
+import {MikiPersonService} from '../../providers/miki-person';
+import {MikiPubliciteService} from '../../providers/miki-publicite';
+
+
+@Component({
+  templateUrl: 'accueil.html',
+  selector: 'accueil',
+})
+export class Accueil{
+
+  // private user: boolean = false;
+  private loginPage: any = Login;
+  private createAccountPage: any = AccountCreate;
+  private accountEdit: any = AccountEdit;
+  // private accountEventsList: any = AccountEventsList;
+  private EventsList: any = EventsList;
+
+  constructor(private nav: NavController, private alertCtrl: AlertController, private events: Events, public mikiPerson: MikiPersonService, public mikiPublicite: MikiPubliciteService) {
+    
+    // vérifie si un utilisateur est connecté
+    this.mikiPerson.checkConnection();
+
+
+    // lorsqu'une personne est authentifiée
+    this.events.subscribe('user:login', () => {
+      this.mikiPerson.refreshUser();
+    });
+
+    // lorsqu'un compte utilisateur est modifié
+    this.events.subscribe('account:updated', () => {
+      // rafraichit l'utilisateur puis ouvre une page
+      this.mikiPerson.refreshUser();
+    });
+  }
+
+
+
+  // décnnection
+  disconnect(){
+    let confirm = this.alertCtrl.create({
+      title: 'Déconnection',
+      message: 'Êtes-vous sûr de vouloir vous déconnecter ?',
+      buttons: [
+        {
+          text: 'Oui',
+          handler: () => {
+            this.mikiPerson.disconnect();
+          }
+        },
+        {
+          text: 'Non',
+          handler: () => {}
+        }
+      ]
+    });
+
+    confirm.present();
+  }
+
+
+
+  goto(number: number){
+    switch(number){
+      case 1:
+        // formation continue
+
+        // si des publicités sont configurées on les affiche
+        if (this.mikiPublicite.publicites != undefined && this.mikiPublicite.publicites.fc_home.length > 0){
+          this.nav.push(PubliciteHome, { pubType: 'fc_home', user: this.mikiPerson.user });
+        }
+        else{
+          this.nav.push(this.EventsList, { idCategory: 1, user: this.mikiPerson.user });
+        }
+
+        break;
+      case 2:
+        // 1ers secours
+        
+        // si des publicités sont configurées on les affiche
+        if (this.mikiPublicite.publicites != undefined && this.mikiPublicite.publicites.ps_home.length > 0){
+          this.nav.push(PubliciteHome, { pubType: 'ps_home', user: this.mikiPerson.user });
+        }
+        else{
+          this.nav.push(this.EventsList, { idCategory: 15, user: this.mikiPerson.user });
+        }
+
+        break;
+      case 3:
+        // login
+        this.nav.push(this.loginPage, { user: this.mikiPerson.user });
+        break;
+      case 4:
+        // création d'un compte utilisateur
+        this.nav.push(this.createAccountPage, { user: this.mikiPerson.user });
+        break;
+      case 5:
+        // mon compte
+        this.nav.push(this.accountEdit, { user: this.mikiPerson.user });
+        break;
+      case 6:
+        // déconnexion
+        this.disconnect();
+        break;
+    }
+  }
+}
---
+++ src/providers/miki-events.ts
@@ -0,0 +1,120 @@
+import {Injectable} from '@angular/core';  
+import {Http, Headers} from '@angular/http';
+
+@Injectable()
+export class MikiEventsService {
+
+  constructor(private http: Http) {
+	}
+
+
+
+  /**
+   * Récupère les events. Permet de définir l'id de la catégorie à récupérer. Si vide, récupert tous les events
+   *
+   * type : 1 = tous les events à venir; 2 = les events de l'année en cours (passés et à venir); 3 = les events de l'année prochaine
+   *
+   * public : true = récupère les événements de visibilité publique + privée
+   *          false = récupère uniquement les événements de visibilité publique
+   */
+  getEvents(type: number, isPublic: any, idCategory?: any) {
+    if (isPublic) {
+      isPublic = 1;
+    }
+    else {
+      isPublic = 0;
+    }
+
+    if (idCategory != undefined && idCategory != '') {
+      return this.http.get('http://es-asur.ch/api/index.php/events/list/' + type + '/' + isPublic + '/' + idCategory);
+    }
+    else {
+      return this.http.get('http://es-asur.ch/api/index.php/events/list/' + type + '/' + isPublic);
+    }
+  }
+
+
+
+  /**
+   * Récupère les events auquel une personne donnée participe. Permet de définir l'id de la catégorie à récupérer. Si vide, récupert tous les events
+   *
+   * type : 1 = tous les events; 2 = les events passés; 3 = les events à venir
+   */
+  getEventsFromPerson(idPerson: number, type: number, idCategory?: any) {
+    if (idCategory != undefined && idCategory != '') {
+    return this.http.get('http://es-asur.ch/api/index.php/events/person/list/' + idPerson + '/' + type + '/' + idCategory);
+    }
+    else {
+    return this.http.get('http://es-asur.ch/api/index.php/events/person/list/' + idPerson + '/' + type + '/');
+    }
+  }
+
+
+
+  getEvent(event_id: number) {
+    return this.http.get('http://es-asur.ch/api/index.php/event/' + event_id);;
+  }
+
+
+
+  isSubscribed(event_id: number, user_id: number){
+    return this.http.get('http://es-asur.ch/api/index.php/event/' + event_id + '/is_subscribed/' + user_id);
+  }
+
+
+
+  getCodesPros(){
+    return this.http.get('http://es-asur.ch/api/index.php/events/infos/codes_pros');
+  }
+
+
+
+  getCountries(){
+    return this.http.get('http://es-asur.ch/api/index.php/countries/list');
+  }
+
+
+
+  getCategoriesFromType(type: string){
+    return this.http.get('http://es-asur.ch/api/index.php/categories/' + type);
+  }
+
+
+
+  // inscrit une personne à un événement
+  subscribe(eventId, personId, datas): any{
+    let params = "";
+
+    params += 'eventId=' + eventId + '&personId=' + personId;
+    params += '&params=' + JSON.stringify(datas);
+
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+
+    // retourne une promise
+    return new Promise((resolve, reject) => {
+
+      this.http.post('http://es-asur.ch/api/index.php/events/subscribe/', params, { headers: headers }).subscribe(
+        data => {
+          resolve(data.json());
+        },
+        err => {
+          console.error(err);
+          resolve(datas);
+        }/*,
+        () => console.log('Authentication Complete')*/
+      );   
+    });
+  }
+
+
+
+  // retourne la date formatée correctement
+  getDate(date): string{
+    let dateTemp = date.split(" ");
+    dateTemp = dateTemp[0];
+    dateTemp = dateTemp.split("-");
+    return dateTemp[2] + "/" + dateTemp[1] + "/" + dateTemp[0];
+  }
+}
